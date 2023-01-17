b2bf615f4c59cd328d2ca08a440b9e6f952c8f58
Eugene Otieno <otienoeo@gmail.com>
2023-01-17 11:10:36 +0300

    Setup menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,19 @@
 fun main() {
+    var showMenu = true
+    while (showMenu) {
+        println("Task (hide, show, exit):")
+        when(val action = readln()) {
+            "hide" -> {
+                println("Hiding message in image.")
+            }
+            "show" -> {
+                println("Obtaining message from image.")
+            }
+            "exit" -> {
+                showMenu = false
+                println("Bye!")
+            }
+            else -> println("Wrong task: $action")
+        }
+    }
 }
