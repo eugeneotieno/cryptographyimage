dc7ff1a0067fa352b7809b01948066303b9be3be
Eugene Otieno <otienoeo@gmail.com>
2023-01-22 22:47:52 +0300

    Hide a message

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,88 +1,96 @@
 import java.awt.Color
+import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
+private const val INPUT_ERROR = "Can't read input file!"
+private val END = listOf(0, 0, 3)
 fun main() {
-    var showMenu = true
-    while (showMenu) {
-        println("Task (hide, show, exit):")
-        when(val action = readln()) {
-            "hide" -> {
-                println("Input image file:")
-                val inputName = readln()
+    var command = getCommand()
+    while (command != "exit") {
+        runCommand(command)
+        command = getCommand()
+    }
+    println("Bye!")
+}
 
-                println("Output image file:")
-                val outputName = readln()
+private fun getCommand() = getString("Task (hide, show, exit):")
 
-                println("Message to hide:")
-                val msg = readln()
+private fun runCommand(command: String) {
+    when (command) {
+        "hide" -> hide()
+        "show" -> show()
+        else -> "Wrong task: $command"
+    }.let { println(it) }
+}
 
-                try {
+private fun hide(): String {
+    val input = getFile("Input")
+    val output = getFile("Output")
+    val image = getBufferedImage(input) ?: return INPUT_ERROR
+    val userMessage = getBits()
+    val enoughSpace = image.width * image.height >= userMessage.length
+    val notLargeEnough = "The input image is not large enough to hold this message."
+    val message = if (enoughSpace) "Message saved in ${output.name} image." else return notLargeEnough
+    val save = { if (saveImage(image, output)) message else "Can't write to output file!" }
+    var index = 0
 
-                    val bufferedImage = ImageIO.read(File(inputName))
-
-                    val encodedMsg = msg.encodeToByteArray()
-                    val binaryList = convertToBinary(encodedMsg)
-                    binaryList.add("00000000")
-                    binaryList.add("00000000")
-                    binaryList.add("00000011")
-
-                    val bitArray = mutableListOf<String>()
-                    repeat(binaryList.size) { index->
-                        val bits = binaryList[index].split("")
-                        for (bit in bits) {
-                            if (bit.isNotBlank()) bitArray.add(bit)
-                        }
-                    }
-
-                    val bitsSize = bufferedImage.width * bufferedImage.height
-                    if (bitsSize >= bitArray.size) {
-                        for (x in 0 until bufferedImage.width) {
-                            for (y in 0 until bufferedImage.height) {
-                                val color = Color(bufferedImage.getRGB(x, y))
-                                val red = color.red or 1
-                                val green = color.green or 1
-                                val blue = color.blue or 1
-                                val rgb = Color(red, green, blue).rgb
-                                bufferedImage.setRGB(x,y, rgb)
-                            }
-                        }
-                        ImageIO.write(bufferedImage, "png", File(outputName))
-                        println("Image $outputName is saved.")
-                    } else {
-                        println("The input image is not large enough to hold this message.")
-                    }
-
-
-                } catch (e: Exception){
-                    println("Input Image: $inputName")
-                    println("Output Image: $outputName")
-                    println("Can't read input file!")
-                }
-            }
-            "show" -> {
-                println("Obtaining message from image.")
-            }
-            "exit" -> {
-                showMenu = false
-                println("Bye!")
-            }
-            else -> println("Wrong task: $action")
+    for (y in 0 until image.height) {
+        for (x in 0 until image.width) {
+            if (index == userMessage.length) return save()
+            val bit = userMessage[index++].digitToInt()
+            val color = Color(image.getRGB(x, y)).let { Color(it.red, it.green, it.blue.and(254).or(bit)) }
+            image.setRGB(x, y, color.rgb)
         }
+    }
+    return save()
+}
+
+private fun show(): String {
+    val input = getFile("Input")
+    val image = getBufferedImage(input) ?: return INPUT_ERROR
+    var lastThree = listOf(1, 1, 1)
+    var lastEight = ""
+    var message = ""
+
+    for (y in 0 until image.height) {
+        for (x in 0 until image.width) {
+            lastEight += Color(image.getRGB(x, y)).blue % 2
+            if (lastEight.length == 8) {
+                val code = lastEight.toInt(2)
+                message += code.toChar()
+                lastThree = lastThree.drop(1) + code
+                if (lastThree == END) return "Message:\n" + message.dropLast(3)
+                lastEight = ""
+            }
+        }
+    }
+    return "No message was hidden!"
+}
+
+private fun getBits() = (getString("Message to hide:").map { it.code } + END).joinToString("") {
+    it.toString(2).padStart(8, '0')
+}
+
+private fun getFile(type: String) = File(getString("$type image file:"))
+
+private fun getBufferedImage(file: File): BufferedImage? {
+    return try {
+        ImageIO.read(file)
+    } catch (e: Exception) {
+        null
     }
 }
 
-fun convertToBinary(byteArray: ByteArray): MutableList<String> {
-    val listArray = byteArray.map { Integer.toBinaryString(it.toInt() and 0xFF) }.toMutableList()
-    for (i in listArray.indices) {
-        var pad = ""
-        val padLength = 8 - listArray[i].length
-        repeat(padLength) {
-            pad += "0"
-        }
-        if (pad.isNotBlank()) {
-            listArray[i] = "$pad${listArray[i]}"
-        }
+private fun saveImage(image: BufferedImage, file: File): Boolean {
+    return try {
+        ImageIO.write(image, "png", file)
+    } catch (e: Exception) {
+        false
     }
-    return listArray
+}
+
+private fun getString(message: String): String {
+    println(message)
+    return readln()
 }
